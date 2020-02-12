36673590c20a73160a7a2db52fe8af4bcaa691a2
Gourav <gsharma@carbonite.com>
2020-02-12 23:52:44 +0530

    Dropdown Changes

--- ats-web/src/components/shared/Dropdown.js
+++ ats-web/src/components/shared/Dropdown.js
@@ -4,21 +4,50 @@ import PropTypes from "prop-types";
 
 function Dropdown(props) {
     return (
-        <div style={{width:"200px"}}>
-            <Select id={props.id} className={props.Class} onChange={props.onChange}
-                options = {props.arr}
-                isSearchable
-            >
-            </Select>
+         <div className={props.error ? props.containerErrorClass : props.containerClass}>
+             <div className={props.divLabelClass}>
+             <label htmlFor={props.id} className={props.isRequired ? props.requiredLabelClass : props.labelClass}>{props.label}</label>
+             </div>
+            <div className={props.divSelectClass}>
+                <Select id={props.id}
+                    name={props.name}
+                    defaultValue={props.arr[0]}
+                    onChange={props.onChange}
+                    options={props.arr}
+                    isSearchable
+                />
+                {props.error && (
+                    <span className="help-block">{props.error}</span>)}
+            </div>
         </div>
-    )
+    );
 }
 
 Dropdown.propTypes = {
     id: PropTypes.string.isRequired,
-    Class: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
     onChange: PropTypes.func.isRequired,
-    arr: PropTypes.array.isRequired
+    arr: PropTypes.array.isRequired,
+    error: PropTypes.string,
+    containerErrorClass: PropTypes.string,
+    containerClass: PropTypes.string,
+    Class: PropTypes.string,
+    requiredLabelClass: PropTypes.string,
+    labelClass: PropTypes.string,
+    divLabelClass: PropTypes.string,
+    divSelectClass: PropTypes.string
+};
+
+Dropdown.defaultProps = {
+    error: "",
+    containerClass: "col-sm-6",
+    containerErrorClass: "col-sm-6 has-error",
+    labelClass: "control-label",
+    requiredLabelClass: "control-label required",
+    Class: "form-control",
+    divLabelClass: "col-sm-2",
+    divSelectClass: "col-sm-4"
 };
 
 export default Dropdown;
