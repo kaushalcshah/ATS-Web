68907a71993dd8c90b6439dfe1fdb85bcb798902
sanyam007 <sanyamchhabra143@gmail.com>
2020-02-17 12:10:54 +0530

    updated leftBar

---
+++ ats-web/src/components/Candidates/Candidates.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Candidates() {
+    return (
+        <div>
+            <h1>Candidates</h1>
+        </div>
+    )
+}
+export default Candidates;
--- ats-web/src/components/shared/NavBar.js
+++ ats-web/src/components/shared/NavBar.js
@@ -1,80 +1,61 @@
 import React, { Component } from 'react';
-import { Layout, Menu, Icon } from 'antd';
+import { Layout, Menu, Icon, Collapse } from 'antd';
 import image from '../shared/niyuj_logo.png'
 import image1 from '../shared/niyuj.jpg';
-import 'antd/dist/antd.css';
-import Home from '../Home';
-import Candidates from '../Candidates';
-import Positions from '../Positions';
-import Schedules from '../Schedules';
+import Home from '../Home/Home';
+import Candidates from '../Candidates/Candidates';
+import Positions from '../Positions/Positions';
+import Schedules from '../Schedules/Schedules';
 import { BrowserRouter as Router, Switch, Route, NavLink } from "react-router-dom";
 const { Header, Sider, Content } = Layout;
 
 class NavBar extends React.Component {
-    siderWidth = "200px";
-    collapsableWidth = "80px";
+  
     state = {
-        collapsed: false,
-        showImage: image,
-        imgCss: "img"
+        collapsed:"false"
     };
 
-
     toggle = () => {
-        let finalImage;
-        let finalCss;
-        if (this.state.showImage === image) {
-            finalImage = image1;
-            finalCss = "img1";
-        }
-        else {
-            finalImage = image;
-            finalCss = "img";
-        }
         this.setState({
             collapsed: !this.state.collapsed,
-            showImage: finalImage,
-            imgCss: finalCss
         });
     };
 
-
-    render() {
-        const { match, location, history } = this.props;
-
+    
+    render() {  
         return (
             <Router>
                 <div className="row" >
                     <Layout>
-                        <div style={{ width: this.state.collapsed ? this.collapsableWidth : this.siderWidth, transition: "all 0.2s" }}>
+                        <div className={this.state.collapsed ? "collapsableWidth" : "siderWidth"}>
                             <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.toggle}
-                                collapsible={true} width={this.siderWidth} collapsedWidth={this.collapsableWidth} style={{ height: "100vh", position: "fixed", left: 0 }}>
+                                collapsible={true} className="sider">
                                 <div className="logo" >
-                                    <img src={this.state.showImage} className={this.state.imgCss} />
+                                    <img src={this.state.collapsed ? image1:image} className={this.state.collapsed ? "img1":"img"} />
                                 </div>
 
-                                <Menu theme="dark" mode="inline" selectedkeys={[]} selectable={false}>
+                                <Menu theme="dark" mode="inline" selectable={false}>
                                     <Menu.Item key="1">
-                                        <NavLink to="/" exact activeStyle={{ color: "yellow" }}>
+                                        <NavLink exact to="/" >
                                             <Icon type="home" />
                                             <span>Home</span>
                                         </NavLink>
                                     </Menu.Item>
                                     <Menu.Item key="2">
-                                        <NavLink to="/positions" exact activeStyle={{ color: "yellow" }}>
+                                        <NavLink exact to="/positions"  >
                                             <Icon type="idcard" />
                                             <span>Positions</span>
                                         </NavLink>
                                     </Menu.Item>
 
                                     <Menu.Item key="3">
-                                        <NavLink to="/candidates" exact activeStyle={{ color: "yellow" }}>
+                                        <NavLink exact to="/candidates">
                                             <Icon type="user" />
                                             <span>Candidates</span>
                                         </NavLink>
                                     </Menu.Item>
                                     <Menu.Item key="4">
-                                        <NavLink to="/schedules" exact activeStyle={{ color: "yellow" }}>
+                                        <NavLink exact to="/schedules">
                                             <Icon type="schedule" />
                                             <span>Schedules</span>
                                         </NavLink>
@@ -84,8 +65,8 @@ class NavBar extends React.Component {
                             </Sider>
                         </div>
                         <div className="col-md-10">
-                            <Layout>
-                                <Content style={{ margin: "20px" }} >
+                            <Layout className="layout">
+                                <Content className="content" >
                                     <Switch>
                                         <Route exact path="/">
                                             <Home />
